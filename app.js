4241adb3a02a2e449e4c3037bf1c66ba7368b4b3
IssacHarmon <isac_gnar@yahoo.com>
2020-05-11 21:40:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let friends = ["Taylor", "Kenny", "Des", "Abita", "Brandon"];
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i] + ":");
+    for (let j = 99; j >= 1; j--) {
+        if (j > 2)
+            console.log(
+                j +
+                " lines of codes in the file, " +
+                j +
+                " lines of code; " +
+                friends[i] +
+                " strikes one out, clears it all out, " +
+                (j - 1) +
+                " lines of code in the file."
+            );
+        else if (j === 2)
+            console.log(
+                j +
+                " line of code in the file, " +
+                j +
+                " line of code; " +
+                friends[i] +
+                " strikes ones out, clears it all out,  " +
+                (j - 1) +
+                " line of code in the file."
+            );
+        else if (j === 1)
+            console.log(
+                j +
+                " line of code in the file, " +
+                j +
+                " line of code; " +
+                friends[i] +
+                " strikes ones out, clears it all out,  " +
+                "no more lines of code in the file."
+            );
+    }
+}
+
+function myFunction() {
+    alert("That was rude!");
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This was my attempt at adding a good bit of styling to a simple portion of the
+page to test my skillset I have learned already.
+Took me a good bit of time but it was worth the effort.
